60924fbc742a5080d943ff31748e6b0315badf1d
Jeff Newman <jeff@driftless.xyz>
2024-12-04 21:16:34 -0600

    configurable est mode setup

--- activitysim/examples/example_estimation/notebooks/est_mode_setup.py
+++ activitysim/examples/example_estimation/notebooks/est_mode_setup.py
@@ -14,7 +14,27 @@ warnings.filterwarnings(
 )
 
 
-def prepare() -> Path:
+def prepare(
+    household_sample_size: int = 20_000, subdir: str = "test-estimation-data"
+) -> Path:
+    """Prepare the example for estimation.
+
+    This function prepares the example for estimation by downloading the example data and
+    setting up the working directory. The current working directory is then set to the
+    created example directory.
+
+    Parameters
+    ----------
+    household_sample_size : int, optional
+        The number of households to sample from the synthetic population. The default is 20_000.
+    subdir : str, optional
+        The subdirectory to store the example data. The default is "test-estimation-data".
+
+    Returns
+    -------
+    Path
+        The path to the created example directory.
+    """
     root_dir = Path(__file__).parent.parent.resolve()
     sys.path.insert(0, str(root_dir))
 
@@ -27,12 +47,9 @@ def prepare() -> Path:
         )
         raise
 
-    as_needed(root_dir / "notebooks" / "test-estimation-data", 20_000)
+    as_needed(root_dir / "notebooks" / subdir, household_sample_size)
     relative_path = os.path.relpath(
-        root_dir
-        / "notebooks"
-        / "test-estimation-data"
-        / "activitysim-prototype-mtc-extended"
+        root_dir / "notebooks" / subdir / "activitysim-prototype-mtc-extended"
     )
     os.chdir(relative_path)
     return Path(relative_path)
