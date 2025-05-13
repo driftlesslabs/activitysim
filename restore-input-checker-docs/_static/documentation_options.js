6d7516cab6d5bc829d847f37cd0f8d9fba7afeeb
jpn-- <jpn--@users.noreply.github.com>
2025-05-13 21:55:14 +0000

    deploy: 96f7d1ab68524638eaacd3384def5776c4f7d2c1

---
+++ restore-input-checker-docs/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.3.3.dev16+g96f7d1ab',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
