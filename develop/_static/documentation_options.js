f488a2b8771de7a3f6d90b3a9d2573fe2c1b7144
jpn-- <jpn--@users.noreply.github.com>
2025-09-11 17:48:06 +0000

    deploy: 7f80635cc64f029be3ed5eb18ae585c57a23a135

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.1.dev12+g092c6e994',
+    VERSION: '1.4.1.dev14+g7f80635cc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
