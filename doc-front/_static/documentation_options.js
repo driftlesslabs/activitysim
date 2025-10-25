c56147fc1d32517fefff3df4eea05c860ceb9502
jpn-- <jpn--@users.noreply.github.com>
2025-10-25 14:28:39 +0000

    deploy: 599aaeae88a9479235d1a89bb4dfdaf2ddac7a47

--- doc-front/_static/documentation_options.js
+++ doc-front/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.1.dev2+gfffc21fe8',
+    VERSION: '1.5.1.dev3+g599aaeae8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
