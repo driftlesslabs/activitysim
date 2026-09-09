3504295353e652bd38644482470ab51d7524eb31
jpn-- <jpn--@users.noreply.github.com>
2026-09-09 01:49:26 +0000

    deploy: 96d2b5e7b15ab7730a592b44602eaa439bf512c9

---
+++ docs/park-and-ride-1001/_static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.5.2.dev35+g96d2b5e7b',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
